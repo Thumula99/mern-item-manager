81063995c636304b63d6a05538ee8137c5337f1f
IT24100476 <it24100476@my.sliit.lk>
2026-04-27 18:35:13 +0530

    t

--- backend/server.js
+++ backend/server.js
@@ -26,5 +26,5 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.log(err));
 const itemRoutes = require('./routes/items');
 app.use('/api/items', itemRoutes);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
